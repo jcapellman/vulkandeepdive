36dc5502072045cfd2b8d83e0071602b53158941
Jarred Capellman <jcapellman@hotmail.com>
2017-09-24 15:29:01 -0500

    Added: Day 01 Code

---
+++ src/day01/Main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <iostream>
+
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "vulkan-1.lib")
