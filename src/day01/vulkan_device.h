b1d694882e9dbf5922faad38624c3a9e1604579d
Jarred Capellman <jcapellman@hotmail.com>
2017-10-17 22:20:50 -0500

    Added: Extension method for Vulkan

--- src/day01/vulkan_device.h
+++ src/day01/vulkan_device.h
@@ -10,6 +10,21 @@ class vulkan_device
 			return string(m_physical_device_properties_.deviceName);
 		}
 
+		vector<string> get_extensions() const
+		{
+			uint32_t extensionCount = 0;
+			vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+			vector<VkExtensionProperties> extensions(extensionCount);
+			vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+			vector<string> extension_names;
+
+			for (const auto& extension : extensions) {
+				extension_names.insert(extension.extensionName);
+			}
+
+			return extension_names;
+		}
+
 		explicit vulkan_device(const VkPhysicalDevice physical_device): m_device_(nullptr)
 		{
 			m_physical_device_ = physical_device;
